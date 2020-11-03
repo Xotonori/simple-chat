cf6502ebacb83c5feb4996907da6a28eec9e8e25
Xotonori <xotonori29011991@gmail.com>
2020-11-03 03:26:45 +0300

    created server for socket

---
+++ server.js
@@ -0,0 +1,20 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const rooms = new Map();
+
+app.get('/rooms', (req, res) => {
+    res.json(rooms);
+});
+
+io.on('connection', socket => {
+    console.log('user connected', socket)
+})
+
+http.listen(9999, (err) => {
+    if (err) {
+        throw Error(err);
+    }
+    console.log('server is working');
+});
